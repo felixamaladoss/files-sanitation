1290830b24514ea2b73ff1abf96df049bbb8f81b
Felix Xavier Amaladoss <famaladoss@ebi.ac.uk>
2021-03-13 20:17:24 +0000

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
-# files-sanitation
+## Files Sanitation
 Bash script that removes/replaces special characters in filenames
+
+# Syntax
+
+```
+./sanitize_filenames.sh [ARGS]
+``` 
+File list as argument. Comma separated values with double quotes prefixed and suffixed.
+
+For example:
+
+```
+./sanitize_filenames.sh "analysis{fir #.py,rep>ort}2020.xl,one\\eml/u&.lp,two=sta+ts.sl,final\`repo.ui"
+``` 
+
+# Escaping
+
+Following special characters should be escaped with backslash while giving as a input. 
+
+!(exclamation) `(backtick) "(double quotes) \(backslash)
+
+```
+\! \` \" \\
+``` 
+
+# Replacement
+
+Following special characters will be replaced with corresponding texts.
+
+```
+# pound
+% percent
+& ampersand
+{ leftCurlBr
+} rightCurlBr
+< leftBr
+> rightBr
+* asterisk
+? question
+! exclam
+: colon )
+@ at
++ plus
+| pipe
+= eq
+```
+
+Following special characters will be removed
+
+```
+blank space
+\(backslash)
+'(single quote)
+"(double quotes)
+
+
+
+
+
+
+
+
---
+++ sanitize_filenames.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+# Author : Felix Xavier
+# Replace or remove special character in given list of file names
+
+if [ -z "$1" ]
+then
+  echo "Please provide the arguments :- file names with comma separated values and double quotes prefixed and suffixed."
+  echo "For Example: ./sanitize_filenames.sh \"analysis%fir#.py,report@2020.xl\""
+
+else
+   echo "Files: $1"
+#
+   FILES_STRING="$(echo -e "${1}" | tr -d '[:space:]')"
+
+   FILES_STRING="$(echo -e "${FILES_STRING}" | tr -d '"')"
+
+   FILES_STRING="$(echo -e "${FILES_STRING}" | tr -d "'")"
+
+   declare -A replacements
+
+   replacements["#"]=pound
+
+   replacements+=( ["%"]=percent ["&"]=ampersand ["{"]=leftCurlBr ["}"]=rightCurlBr ["<"]=leftBr [">"]=rightBr )
+   replacements+=( ["*"]=asterisk ["?"]=question ["!"]=exclam [":"]=colon )
+   replacements+=( ["@"]=at ["+"]=plus ["|"]=pipe ["="]=eq )
+
+#
+# Do find and replace on filestring while reading
+# replacements bash array
+#
+
+  for key in "${!replacements[@]}"
+  do
+     #echo $key
+     replace=${replacements[${key}]}
+     #echo $replace
+     FILES_STRING=$(echo $FILES_STRING | sed "s/${key}/${replace}/g")
+     #echo $FILES_STRING
+  done
+
+  FILES_STRING=$(echo $FILES_STRING | sed "s/\`/backTick/g")
+  FILES_STRING=$(echo $FILES_STRING | sed "s#/#forSlash#g")
+  FILES_STRING=$(echo $FILES_STRING | sed 's#\\#backSlash#g')
+
+
+  #echo "File string after replace $FILES_STRING"
+#
+  IFS=',' read -r -a fileArray <<< "$FILES_STRING"
+
+  echo "File list :-"
+  for element in "${fileArray[@]}"
+  do
+      echo "$element"
+  done
+fi
